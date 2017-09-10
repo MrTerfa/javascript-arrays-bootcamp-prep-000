3d493c9090694594b9d34e4c2b3befdc92cd2fd1
Terfa Tortema <terfa.tortema@gmail.com>
2017-09-10 22:07:24 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,43 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+
+
+var addElementToBeginningOfArray = function (array,element){
+  var newArray = [element,...array];
+  return newArray;
+}
+
+var destructivelyAddElementToBeginningOfArray = function (array,element){
+  array = array.unshift(element);
+  return array;
+}
+/*
+var addElementToEndOfArray = function (array,element){
+  var newArray = [array...,element];
+  return newArray;
+}
+
+var destructivelyAddElementToEndOfArray = function (array,element){
+  return array.push(element);
+}
+
+var accessElementInArray = function (array,index){
+  return array[index];
+}
+
+var destructivelyRemoveElementFromBeginningOfArray = function (array){
+  return array.shift();
+}
+
+var removeElementFromBeginningOfArray = function (array){
+  var newArray = array.slice(1);
+}
+
+var destructivelyRemoveElementFromEndOfArray = function (array){
+  return array.pop();
+}
+
+var removeElementFromEndOfArray = function (array){
+  var newArray  = array.slice(0,array.length-1);
+  return newArray;
+}
+*/
